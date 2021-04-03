67a6d679eef66d2be51dad5bd9efc02fe9fab60c
Matt Jones <gitcode@magisa.org>
2021-04-02 19:55:40 -0800

    Show volume management commands

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -3,6 +3,14 @@
 # Create a ConfigMap with our ENV VARS. Be sure to edit the .env file first
 kubectl create configmap timedb-config --from-env-file=.env
 
+# Set up perrsistent volumes for the postgres DB
+kubectl apply -f timedb-pv.yaml
+kubectl get pv timedb-volume
+kubectl apply -f timedb-pvc.yaml
+kubectl get pv timedb-volume
+kubectl get pvc timedb-pv-claim
+
+# Create the Timedb deployment
 kubectl apply -f timedb-deployment.yaml
 kubectl get deployments,services,pods --show-labels=true
 
@@ -13,4 +21,4 @@ kubectl get deployments,services,pods --show-labels=true
 kubectl logs pod/timescaledb-575d984dcd-4dmwf
 
 # To connect to a shell in a pod, use:
-kubectl exec pod/timescaledb-575d984dcd-4dmwf -i -t bash
+kubectl exec pod/timescaledb-575d984dcd-4dmwf -i -t -- bash
